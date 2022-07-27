0a76b62da45f0352cfb0c4dc52494ee297bb900d
Oleksandr <96943240+Alex19919@users.noreply.github.com>
2022-07-27 16:04:23 +0300

    some try

--- twoSum.py
+++ twoSum.py
@@ -1,4 +1,3 @@
-from typing import List
 
 # Дано масив цілих чисел nums і ціле число,
 # поверніть індекси двох чисел так, щоб їх сума дала передане ціле число.
@@ -9,11 +8,35 @@ from typing import List
 # Ви можете повернути відповідь у будь-якому порядку.
 
 
-def twoSum(nums: List[int], target: int) -> List[int]:
-    print(nums, target)
+
+def twoSum(nums, target):
+    diction = {}
+    for i, num in enumerate(nums):
+        m = target - num
+        if m in diction:
+            return print([diction[m], i])
+        else:
+            diction[num] = i
 
 
 twoSum([1, 2, 3], 4)        # [0, 2]
 twoSum([2, 7, 11, 15], 9)   # [0, 1]
 twoSum([3, 2, 4], 6)        # [1, 2]
 twoSum([3, 3], 6)           # [0, 1]
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    #	print(nums, target)
+	result = {}
+	for i in range(len(nums)):
+		if target - nums[i] in result:
+			return [result[target - nums[i]], i]
+		else:
+			result[nums[i]] = i
+
+
+print(twoSum([1, 2, 3], 4))        # [0, 2]
+twoSum([2, 7, 11, 15], 9)   # [0, 1]
+twoSum([3, 2, 4], 6)        # [1, 2]
+twoSum([3, 3], 6)           # [0, 1]
+
