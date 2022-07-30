fd8d7eff1fa5fe4ab65804e628373a65a68dd78e
Oleksandr <96943240+Alex19919@users.noreply.github.com>
2022-07-30 10:47:48 +0300

    NewOne

--- twoSum.py
+++ twoSum.py
@@ -8,7 +8,6 @@
 # Ви можете повернути відповідь у будь-якому порядку.
 
 
-
 def twoSum(nums, target):
     diction = {}
     for i, num in enumerate(nums):
@@ -23,20 +22,3 @@ twoSum([1, 2, 3], 4)        # [0, 2]
 twoSum([2, 7, 11, 15], 9)   # [0, 1]
 twoSum([3, 2, 4], 6)        # [1, 2]
 twoSum([3, 3], 6)           # [0, 1]
-
-
-def twoSum(nums: List[int], target: int) -> List[int]:
-    #	print(nums, target)
-	result = {}
-	for i in range(len(nums)):
-		if target - nums[i] in result:
-			return [result[target - nums[i]], i]
-		else:
-			result[nums[i]] = i
-
-
-print(twoSum([1, 2, 3], 4))        # [0, 2]
-twoSum([2, 7, 11, 15], 9)   # [0, 1]
-twoSum([3, 2, 4], 6)        # [1, 2]
-twoSum([3, 3], 6)           # [0, 1]
-
